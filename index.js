2b34453af4da8c8dba1d118219fc1338bee21382
Adithya kamath <kamathadithya12@gmail.com>
2023-11-13 18:09:08 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,43 @@
+let rows = 100;
+let cols = 26;
+
+let addColCount = document.querySelector(".add-col-container");
+let rowColCount = document.querySelector(".add-row-container");
+let cellsCount = document.querySelector(".cells-container");
+let addBar = document.querySelector(".address-bar");
+
+for (let i = 0; i < rows; i++) {
+  let addCol = document.createElement("div");
+  addCol.setAttribute("class", "add-col");
+  addCol.innerText = i + 1;
+  addColCount.appendChild(addCol);
+}
+
+for (let i = 0; i < cols; i++) {
+  let addRow = document.createElement("div");
+  addRow.setAttribute("class", "add-row");
+  addRow.innerText = String.fromCharCode(65 + i);
+  rowColCount.appendChild(addRow);
+}
+
+for (let i = 0; i < rows; i++) {
+  let addRow = document.createElement("div");
+  addRow.setAttribute("class", "cell-row");
+  for (let j = 0; j < cols; j++) {
+    let cell = document.createElement("div");
+    cell.setAttribute("class", "cell");
+    cell.setAttribute("contenteditable", "true");
+
+    addRow.appendChild(cell);
+    addListnerForAddBarDisplay(cell, i, j);
+  }
+  cellsCount.appendChild(addRow);
+}
+
+function addListnerForAddBarDisplay(cell, i, j) {
+  cell.addEventListener("click", (e) => {
+    let rowID = i + 1;
+    let colID = String.fromCharCode(65 + j);
+    addBar.value = `${colID}${rowID}`;
+  });
+}
